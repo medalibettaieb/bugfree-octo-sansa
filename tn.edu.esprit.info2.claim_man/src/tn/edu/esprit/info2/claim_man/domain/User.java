801a7b0aa2c7db0968e1a4f14075378c5417f4a4
medali.bettaieb <medali.bettaieb@esprit.tn>
2014-05-24 17:44:03 +0200

    Signed-off-by: medali.bettaieb <medali.bettaieb@esprit.tn>

--- tn.edu.esprit.info2.claim_man/src/tn/edu/esprit/info2/claim_man/domain/User.java
+++ tn.edu.esprit.info2.claim_man/src/tn/edu/esprit/info2/claim_man/domain/User.java
@@ -5,6 +5,8 @@ import tn.edu.esprit.info2.claim_man.utilities.IdGenerator;
 public class User {
 	public int id;
 	public String name;
+	public String login;
+	public String password;
 
 	public User() {
 
